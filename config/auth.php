08bde22c1d15554c01425647761bcd2b8c8a72c5
Umair Maqsood <142876620+Umairmaqsood00@users.noreply.github.com>
2025-11-30 21:15:32 +0500

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'api'),
+        'guard' => env('AUTH_GUARD', 'api'), // JWT API guard for WorkerConnect
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
