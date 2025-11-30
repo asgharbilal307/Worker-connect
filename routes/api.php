076e062d78f5b227df42067afa95d48bb43c9b30
Umair Maqsood <142876620+Umairmaqsood00@users.noreply.github.com>
2025-11-30 21:16:02 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,19 @@
 <?php
+// WorkerConnect API Routes
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\WorkerController;
 
-
-
+// Authentication routes
 Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
     Route::post('/logout',  'logout')->middleware('multi-auth');
     Route::post('/refresh', 'refresh')->middleware('multi-auth');
 });
+
+// Customer routes
 Route::controller(CustomerController::class)->prefix('customer')->group(function ()
 {
     Route::get('dashboard','dashboard')->middleware('auth:customer');
@@ -19,6 +21,8 @@ Route::controller(CustomerController::class)->prefix('customer')->group(function
     Route::post('search-worker','SearchWorker')->middleware('auth:customer');
 
 });
+
+// Worker routes
 Route::controller(WorkerController::class)->prefix('worker')->group(function ()
 {
     Route::get('dashboard','dashboard')->middleware('auth:worker');
